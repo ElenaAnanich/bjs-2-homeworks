fedbb9c5b0c7de8536ae21cd3d7ffe28eac3a559
Lena k <ktyecz09@mail.ru>
2023-06-25 18:42:26 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !==arr2.length){
+        return false;
+    }
+  return arr2.every((item,index)=> item ===arr1[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const usersByGender = users.filter((user)=>user.gender === gender);
+    const userAge = usersByGender.map((user)=>user.age);
+    const totalAge =userAge.reduce((acc,age)=>acc + age, 0);
+    if (userAge.length ===0){
+        return 0;
+    }
+  return totalAge/userAge.length
 }
